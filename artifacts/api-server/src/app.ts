cf0d8e5b09e4b60f137cc97ef52c0cf33834c1aa
davidndu419 <ugonnadavidnduka@gmail.com>
2026-04-19 00:22:50 +0100

    fix: explicit pinoHttp import and any type for req/res

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,19 +1,20 @@
-import express, { Express, Request, Response } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http"; // Changed from 'import pinoHttp' to '{ pinoHttp }'
 import router from "./routes";
 import { logger } from "./lib/logger";
+
 const app: Express = express();
 
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req: Request) {
+      req(req: any) { // Changing :Request to :any here temporarily to bypass the Vercel crash
         return {
           id: req.id,
           method: req.method,
-          url: req.url?.split("?")[0],
+          url: req.url?.split("?")[0]
         };
       },
       res(res: Response) {
